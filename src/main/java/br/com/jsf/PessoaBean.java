b2a896513b82f829857f945c6a239661010c8060
mlima3696 <mjuniorlima3@gmail.com>
2020-11-25 00:02:53 -0300

    Ligando atributos com expression language jsf

--- src/main/java/br/com/jsf/PessoaBean.java
+++ src/main/java/br/com/jsf/PessoaBean.java
@@ -4,9 +4,23 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "pessoaBean")
 public class PessoaBean {
-	
+
 	private String nome;
 	private String sobrenome;
+	private String nomeCompleto;
+
+	public String mostrarNome() {
+		nomeCompleto = nome + "--" + sobrenome;
+		return "";
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
 
 	public String getNome() {
 		return nome;
